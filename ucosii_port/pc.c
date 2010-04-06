1bd4bb60bd52135c9b0c63917114cccff17aacf5
Ahmed Ben Messaoud <ahmedbm@gmail.com>
2010-04-06 00:13:49 -0400

    Added ignore file and changed to nix style line endings

--- ucosii_port/pc.c
+++ ucosii_port/pc.c
@@ -11,7 +11,7 @@
    *********************************************************************************************************
  */
 
-#include "../includes.h"
+#include "includes.h"
 
 BOOLEAN lock=FALSE;
 extern void (*interruptTable[])();
